1bb31e813a3ab7f200c6e32c2945a67cf2438968
Keerti Raj <75358425+keertiRaj-dev@users.noreply.github.com>
2022-07-30 13:30:46 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from "react";
+
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+
+function App() {
+const LOCAL_STORAGE_KEY = "contacts";
+
+const retriveContacts = localStorage.getItem(LOCAL_STORAGE_KEY) ?JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) :[];
+const [contacts, setContacts] = useState(retriveContacts);
+
+const addContactHandler = (contact) => {
+  console.log(contact);
+  setContacts([...contacts, contact]);
+};
+useEffect(() => {
+const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+if (retriveContacts)
+{setContacts(retriveContacts);}
+},[]);
+
+useEffect(() =>{
+localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+}, [contacts]);
+
+  return (
+<div className="ui container">
+<Header />
+<AddContact addContactHandler={addContactHandler}/>
+<ContactList contacts={contacts}/>
+  </div>
+  );
+};
+
+export default App;
